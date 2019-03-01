d8a94517ae0e60097a36421d551bb38075b8a22d
Mandy Trinh <saphiric@gmail.com>
2019-02-28 16:41:42 -0800

    Remove extra queries

--- AMP/home-value-module/schema.sql
+++ AMP/home-value-module/schema.sql
@@ -32,26 +32,4 @@ INSERT INTO homes ("zestimationPrice", "startPriceRange", "endPriceRange", "thir
 
 INSERT INTO homes ("zestimationPrice", "startPriceRange", "endPriceRange", "thirtyDayPriceChange", "oneYearForcast", "propertyLastSalePrice", "propertLastSaleDate", "comparableHomePrice", "marketAppreciationPrice", "localSalesAvg", "sellDate", "sellPrice", "beds", "baths", "sqft", "streetAddress", "priceSqft", "saleToList", "url") VALUES ('523,456', '170,000', '320,000', '10,000', '100,000', '800,000', '2/1/1990', '120,000', '50,000', '120,000', 'N/A', 'N/A', 4, 2, '5000', '2222 Yayyay Ave', '300', 99, 'https://s3-us-west-1.amazonaws.com/zillow-talk-home-component/large20.jpg');
 
-INSERT INTO homes ("id", "zestimationPrice", "startPriceRange", "endPriceRange", "thirtyDayPriceChange", "oneYearForcast", "propertyLastSalePrice", "propertLastSaleDate", "comparableHomePrice", "marketAppreciationPrice", "localSalesAvg", "sellDate", "sellPrice", "beds", "baths", "sqft", "streetAddress", "priceSqft", "saleToList", "url")
-    VALUES (1, '323,456', '70,000', '320,000', '10,000', '100,000', '800,000', '2/1/1990', '120,000', '50,000', '120,000', 'N/A', 'N/A', 4, 2, '5000', '2222 Nono Ave', '100', 19, 'https://s3-us-west-1.amazonaws.com/zillow-talk-home-component/large20.jpg')
-    ON CONFLICT ("id") 
-    DO UPDATE SET 
-    "zestimationPrice" = EXCLUDED."zestimationPrice",
-    "startPriceRange" = EXCLUDED."startPriceRange", 
-    "endPriceRange" = EXCLUDED."endPriceRange", 
-    "thirtyDayPriceChange" = EXCLUDED."thirtyDayPriceChange", 
-    "oneYearForcast" = EXCLUDED."oneYearForcast", 
-    "propertyLastSalePrice" = EXCLUDED."propertyLastSalePrice", 
-    "propertLastSaleDate" = EXCLUDED."propertLastSaleDate", 
-    "comparableHomePrice" = EXCLUDED."comparableHomePrice", 
-    "marketAppreciationPrice" = EXCLUDED."marketAppreciationPrice", 
-    "localSalesAvg" = EXCLUDED."localSalesAvg", 
-    "sellDate" = EXCLUDED."sellDate", 
-    "sellPrice" = EXCLUDED."sellPrice", 
-    "beds" = EXCLUDED."beds", 
-    "baths" = EXCLUDED."baths", 
-    "sqft" = EXCLUDED."sqft", 
-    "streetAddress" = EXCLUDED."streetAddress", 
-    "priceSqft" = EXCLUDED."priceSqft", 
-    "saleToList" = EXCLUDED."saleToList", 
-    "url" = EXCLUDED."url";
+  
